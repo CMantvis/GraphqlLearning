a24645ed322c2252de2979ce80a28375c8d08278
CMantvis <civilka.mantas@gmail.com>
2020-07-25 17:59:13 +0300

    Finish graphql schema, connect to mongodb, finish mongodb schema

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require('express-graphql');
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb+srv://Mantvydas:15312047@cluster0.nhfpz.mongodb.net/cluster0?retryWrites=true&w=majority", 
+{ useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connection.once('open', () => {
+  console.log("connected to database");
+});
 
 app.use('/graphql',graphqlHTTP({
     schema,
